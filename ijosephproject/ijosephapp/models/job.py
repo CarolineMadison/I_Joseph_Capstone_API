bbedaf31aaeafe0fda918dd602f7f0b4ae45e190
Caroline Brownlee <brownleecaroline@gmail.com>
2020-03-13 16:09:54 -0500

    changed table

--- ijosephproject/ijosephapp/models/job.py
+++ ijosephproject/ijosephapp/models/job.py
@@ -8,9 +8,8 @@ class Job(models.Model):
     category = models.ForeignKey("JobCategory", on_delete=models.DO_NOTHING)
     location = models.CharField(max_length=20)
     description = models.CharField(max_length=255)
-    isActive = models.BooleanField()
-    isCompleted = models.BooleanField()
-    createdAt = models.DateTimeField(auto_now_add=True)
+    isCompleted = models.BooleanField(default=False)
+    createdAt = models.DateField(auto_now_add=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
