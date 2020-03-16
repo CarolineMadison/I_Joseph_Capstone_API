84e39880137fa6f0ad665eb050fadcab42a61de9
Caroline Brownlee <brownleecaroline@gmail.com>
2020-03-16 09:33:23 -0500

    list of jobs populates

--- ijosephproject/ijosephapp/models/__init__.py
+++ ijosephproject/ijosephapp/models/__init__.py
@@ -1,4 +1,5 @@
 from .job import Job
 from .job_category import JobCategory
 from .user_job import UserJob
+from .modelfactory import model_factory
 
--- ijosephproject/ijosephproject/urls.py
+++ ijosephproject/ijosephproject/urls.py
@@ -1,5 +1,4 @@
 """ijosephproject URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -14,9 +13,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include, path
 from django.urls import path
 from ijosephapp.models import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('ijosephapp.urls')),
 ]
