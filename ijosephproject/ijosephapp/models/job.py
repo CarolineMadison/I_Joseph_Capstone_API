212c984d65a070f14b48304a3ae8e932a0d7988e
Caroline Brownlee <brownleecaroline@gmail.com>
2020-03-15 21:44:05 -0500

    models created, SQL queries in TablesPlus save to the database.

--- ijosephproject/ijosephapp/models/job.py
+++ ijosephproject/ijosephapp/models/job.py
@@ -5,12 +5,12 @@ from .job_category import JobCategory
 class Job(models.Model):
 
     title = models.CharField(max_length=50)
-    category = models.ForeignKey("JobCategory", on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(JobCategory, on_delete=models.DO_NOTHING)
     location = models.CharField(max_length=20)
     description = models.CharField(max_length=255)
     isCompleted = models.BooleanField(default=False)
-    createdAt = models.DateField(auto_now_add=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKeys(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}'
